aeb6b8c8d17ae8293cc39b21a7b16bcaf510d3b0
Will Puckett <willpuckett@gmail.com>
2021-08-23 10:10:51 -0700

    organized HW directory & streamlined layouts &  prettier

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = eleventyConfig => {
+    // Copy our static assets to the output folder
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('Skeleton');
+    eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('images');
+  
+    // Returning something from the configuration function is optional
+    return {
+      dir: {
+        output: '_site'
+      }
+    };
+  };
--- hw2.md
+++
@@ -1,4 +0,0 @@
----
-layout: layouts/hw.njk
----
-# Coming Soon!
