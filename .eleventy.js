75ec8631d44466fa21846a0007c20f83a75c7435
Will Puckett <willpuckett@gmail.com>
2021-10-05 19:07:51 -0700

    switch jquery in npm

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ module.exports = eleventyConfig => {
     // Copy our static assets to the output folder
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('favicon');
-    // eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('node_modules/jquery/dist');
+    eleventyConfig.addPassthroughCopy('node_modules/jquery-ui-dist');
+    // eleventyConfig.addPassthroughCopy('.node_modules/jquery-ui-dist/jquery-ui.theme.min.css');
     // eleventyConfig.addPassthroughCopy('images');
   
     // Returning something from the configuration function is optional
