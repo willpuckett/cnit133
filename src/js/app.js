5d60366cd01a3aecb782691dc2b6a6469bc1bb45
Will Puckett <willpuckett@gmail.com>
2021-10-05 21:37:03 -0700

    reduce jquery-ui css, add swap to font

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 // CSS
 require('../css/normalize.css');
 require('../css/skeleton.css');
-require('jquery-ui/themes/base/all.css');
+require('jquery-ui/themes/base/draggable.css');
+require('jquery-ui/themes/base/tooltip.css');
+// require('jquery-ui/themes/base/all.css');
 require('../css/custom.css');
 
 
